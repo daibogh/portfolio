a3476c2fbdcf036163405825eb09f7134ced25bf
daibogh <daibogh@gmail.com>
2023-04-01 20:37:16 +0200

    chore: remove some extra files feat: added phaser

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+// TODO: rewrite it with jest
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
+import { FC } from 'react';
 import styles from './MainPage.module.css';
-import { TextAnimation } from "../../components/TextAnimation";
+import { TextAnimation } from '../../components/TextAnimation';
 
-const MainPage:FC = () => {
-    return <div className={styles.container}>
-        <h1><TextAnimation text="Hi! My name is Peter" /></h1>
+const MainPage: FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textWrapper}>
+        <TextAnimation text="Hi! My name is Peter." />
+      </div>
     </div>
-}
-export default MainPage
+  );
+};
+export default MainPage;
 // идея для портфолио - сделать анимацию, где я отстреливаю баги
 // как в старой 2д игре про защиту от инопланетных захватчиков
