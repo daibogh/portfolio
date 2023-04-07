b3824e62956cc119cd75e64853a6044f4787a23d
daibogh <daibogh@gmail.com>
2023-04-07 17:51:32 +0200

    chore: separate text animation logic from component

--- src/components/TextAnimation/TextAnimation.tsx
+++ src/components/TextAnimation/TextAnimation.tsx
@@ -1,47 +1,23 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC } from 'react';
 import cn from 'classnames';
 import styles from './TextAnimation.module.css';
+import { useTextAnimation } from './use-text-animation';
 type TextAnimationProps = {
   text: string;
   shouldAnimate?: boolean;
   onTypeEnd?: () => void;
 };
-const MAX_DELAY = 150;
 const noop = () => {};
 const TextAnimation: FC<TextAnimationProps> = ({
   text,
   onTypeEnd = noop,
   shouldAnimate,
 }) => {
-  const [visibleText, setVisibleText] = useState(shouldAnimate ? '' : text);
-  const [shouldHideCursor, setShouldHideCursor] = useState(!shouldAnimate);
-
-  const updateVisibleText = useCallback(
-    (idx: number, options: { timer: any }) => {
-      if (idx >= text.length) {
-        setShouldHideCursor(true);
-        onTypeEnd();
-        return;
-      }
-      options.timer = setTimeout(() => {
-        setVisibleText(text.slice(0, idx + 1));
-        updateVisibleText(idx + 1, options);
-      }, Math.floor(Math.random() * MAX_DELAY));
-    },
-    [text],
-  );
-
-  useEffect(() => {
-    if (!shouldAnimate) return;
-    const timerOptions = { timer: null };
-    updateVisibleText(0, timerOptions);
-    return () => {
-      if (timerOptions.timer) {
-        clearTimeout(timerOptions.timer);
-      }
-    };
-  }, [updateVisibleText]);
-
+  const { visibleText, shouldHideCursor } = useTextAnimation({
+    text,
+    onTypeEnd,
+    shouldAnimate,
+  });
   return (
     <div className={cn(styles.animatedText)}>
       {visibleText}
---
+++ src/components/TextAnimation/use-text-animation.ts
@@ -0,0 +1,45 @@
+import { useState, useCallback, useEffect, useMemo } from 'react';
+const MAX_DELAY = 150;
+
+export const useTextAnimation = ({
+  text,
+  shouldAnimate = false,
+  onTypeEnd,
+}: {
+  text: string;
+  shouldAnimate?: boolean;
+  onTypeEnd: () => void;
+}) => {
+  const [visibleText, setVisibleText] = useState(shouldAnimate ? '' : text);
+  const [shouldHideCursor, setShouldHideCursor] = useState(!shouldAnimate);
+
+  const updateVisibleText = useCallback(
+    (idx: number, options: { timer: any }) => {
+      if (idx >= text.length) {
+        setShouldHideCursor(true);
+        onTypeEnd();
+        return;
+      }
+      options.timer = setTimeout(() => {
+        setVisibleText(text.slice(0, idx + 1));
+        updateVisibleText(idx + 1, options);
+      }, Math.floor(Math.random() * MAX_DELAY));
+    },
+    [text],
+  );
+
+  useEffect(() => {
+    if (!shouldAnimate) return;
+    const timerOptions = { timer: null };
+    updateVisibleText(0, timerOptions);
+    return () => {
+      if (timerOptions.timer) {
+        clearTimeout(timerOptions.timer);
+      }
+    };
+  }, [updateVisibleText]);
+  return useMemo(
+    () => ({ visibleText, shouldHideCursor }),
+    [visibleText, shouldHideCursor],
+  );
+};
